ba348e6abf020d0d573c96976ec561d1add662bc
herrsch <jack0088@me.com>
2020-12-14 20:30:00 +0100

    just in case, added anti-stuck-modifier setting

--- keyboards/kinesis/keymaps/herrsch/config.h
+++ keyboards/kinesis/keymaps/herrsch/config.h
@@ -2,6 +2,7 @@
 
 #define TAPPING_TERM 100 // I'm mainly using this for modifiers, etc
 #define KEY_TAP_TIMEOUT 190 // same meaning as TAPPING_TERM but for the rest of the regular keys (KC_A ... KC_Z)
+#define PREVENT_STUCK_MODIFIERS
 
 //#define STM32_SYSCLK KINETIS_SYSCLK_FREQUENCY
 //#define NOP_FUDGE 0.4
