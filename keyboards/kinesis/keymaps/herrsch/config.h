a50b10f3f4fcf90e2f0928c405a05ff817149663
Geekhunger <jack0088@me.com>
2022-09-01 12:08:42 +0200

    adjusted timer a little bit

--- keyboards/kinesis/keymaps/herrsch/config.h
+++ keyboards/kinesis/keymaps/herrsch/config.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define TAPPING_TERM 90 // I'm mainly using this for modifiers, etc
-#define KEY_TAP_TIMEOUT 160 // same meaning as TAPPING_TERM but for the rest of the regular keys (KC_A ... KC_Z)
+#define KEY_TAP_TIMEOUT 170 // same meaning as TAPPING_TERM but for the rest of the regular keys (KC_A ... KC_Z)
 #define PREVENT_STUCK_MODIFIERS
 
 //#define STM32_SYSCLK KINETIS_SYSCLK_FREQUENCY
